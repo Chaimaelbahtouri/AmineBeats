0878deb3d6e6ab09ff75a1737f2454ce041184a7
chaimaelbahtouri <chaimaaelbahtouri@gmail.com>
2026-07-18 01:48:11 +0100

    connect frontend to deployed backend

--- music-app-frontend/src/api/axios.js
+++ music-app-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://music-app-backend-self.vercel.app/api"
 })
 
 api.interceptors.request.use((config) => {
@@ -12,4 +12,4 @@ api.interceptors.request.use((config) => {
     return config;
 });
 
-export default api
+export default api;
